0d8aa4e076cf020c1f0783eb28402e728e5eaea6
Aswinazikar <azwinaswin8@gmail.com>
2024-08-27 10:07:21 +0530

    "Refactor LoadingButtonV2 to LoadingButton, remove NeopopButton and icon property, update styling and layout"

--- lib/widgets/loading_button.dart
+++ lib/widgets/loading_button.dart
@@ -1,60 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:neopop/neopop.dart';
-import '../exporter.dart';
-
-class LoadingButtonV2 extends StatelessWidget {
-  const LoadingButtonV2({
-    this.icon,
-    super.key,
-    required this.buttonLoading,
-    required this.text,
-    required this.onPressed,
-    this.enabled = true,
-    this.expanded = true,
-    this.textColor = Colors.white,
-    this.backgroundColor = primaryColor,
-    this.padding = const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-  });
-
-  final bool buttonLoading;
-  final Color textColor;
-  final String text;
-  final VoidCallback onPressed;
-  final EdgeInsetsGeometry padding;
-  final bool enabled;
-  final Color backgroundColor;
-  final bool expanded;
-  final Widget? icon;
-
-  @override
-  Widget build(BuildContext context) {
-    Widget button = ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: backgroundColor,
-      ),
-      onPressed: buttonLoading ? null : onPressed,
-      child: Builder(builder: (context) {
-        if (buttonLoading) return const CircularProgressIndicator();
-        final textWidget = Text(text);
-        if (icon != null) {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [icon!, gap, textWidget],
-          );
-        }
-        return textWidget;
-      }),
-    );
-    if (expanded) {
-      return Row(
-        children: [Expanded(child: button)],
-      );
-    } else {
-      return button;
-    }
-  }
-}
 
 class LoadingButton extends StatelessWidget {
   const LoadingButton({
@@ -65,7 +10,7 @@ class LoadingButton extends StatelessWidget {
     this.enabled = true,
     this.expanded = true,
     this.textColor = Colors.white,
-    this.backgroundColor = Colors.black,
+    this.backgroundColor = const Color.fromARGB(255, 83, 192, 10),
     this.padding = const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
   });
 
@@ -80,53 +25,44 @@ class LoadingButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget button = NeoPopButton(
-      
-      enabled: enabled,
-      color: backgroundColor,
-      animationDuration: animationDuration,
-      depth: 1,
-      onTapUp: buttonLoading || !enabled
+    Widget button = ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+        padding: buttonLoading
+            ? const EdgeInsets.symmetric(vertical: 5, horizontal: 20)
+            : padding,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+              12), // Set the radius here for rounded corners
+        ),
+      ),
+      onPressed: buttonLoading || !enabled
           ? null
           : () {
               HapticFeedback.lightImpact();
               onPressed();
             },
-      onTapDown: () => HapticFeedback.mediumImpact(),
-      child: Padding(
-        padding: buttonLoading
-            ? const EdgeInsets.symmetric(vertical: 5, horizontal: 20)
-            : padding,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Builder(builder: (context) {
-              if (buttonLoading) {
-                return Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Visibility(
-                        visible: false,
-                        maintainState: true,
-                        maintainAnimation: true,
-                        maintainSize: true,
-                        child: Text(text, style: TextStyle(color: textColor))),
-                    const FittedBox(child: CircularProgressIndicator()),
-                  ],
-                );
-              }
-              return Text(text, style: TextStyle(color: textColor));
-            }),
-          ],
-        ),
-      ),
+      child: buttonLoading
+          ? SizedBox(
+              width: 24,
+              height: 24,
+              child: CircularProgressIndicator(
+                color: textColor,
+                strokeWidth: 2,
+              ),
+            )
+          : Text(
+              text,
+              style: TextStyle(color: textColor),
+            ),
     );
-    if (!expanded) {
+
+    if (expanded) {
       return Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [button],
+        children: [Expanded(child: button)],
       );
+    } else {
+      return button;
     }
-    return button;
   }
 }
